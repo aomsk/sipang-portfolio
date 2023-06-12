c8c1b0468b2cf6728ef8978d470f7a32a3efa881
Sipang K <plaaom39@gmail.com>
2023-06-12 10:26:29 +0700

    refactor: separate script to index.js"

---
+++ index.js
@@ -0,0 +1,31 @@
+hamburger = document.querySelector(".hamburger");
+hone = document.querySelector(".Hone");
+about = document.querySelector(".About");
+skills = document.querySelector(".Skills");
+projects = document.querySelector(".Projects");
+contact = document.querySelector(".Contact");
+
+hamburger.onclick = function () {
+  navbar = document.querySelector(".nav-bar");
+  navbar.classList.toggle("active");
+};
+hone.onclick = function () {
+  navbar = document.querySelector(".nav-bar");
+  navbar.classList.toggle("active");
+};
+about.onclick = function () {
+  navbar = document.querySelector(".nav-bar");
+  navbar.classList.toggle("active");
+};
+skills.onclick = function () {
+  navbar = document.querySelector(".nav-bar");
+  navbar.classList.toggle("active");
+};
+projects.onclick = function () {
+  navbar = document.querySelector(".nav-bar");
+  navbar.classList.toggle("active");
+};
+contact.onclick = function () {
+  navbar = document.querySelector(".nav-bar");
+  navbar.classList.toggle("active");
+};
